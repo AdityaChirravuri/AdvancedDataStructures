d2142cc9c524a7b4d952d5fd1c970fb46271b9f5
Aditya <adityachirravuri17@gmail.com>
2024-04-07 10:50:24 +0530

    Writing Getters and Setters

--- SkipList/C++/headers/skipList.h
+++ SkipList/C++/headers/skipList.h
@@ -4,7 +4,6 @@
 typedef listNode Node;
 
 class skipList{
-private:
     float p;
     int MAX_LEVEL;
     int level;
@@ -12,14 +11,75 @@ private:
 public:
     skipList(int, float);
     ~skipList();
+    //@@ getters and Setters
+    float skipList::GetP() const {}
+    void skipList::SetP(float p) {}
+    int skipList::GetMAX_LEVEL() const {}
+    void skipList::SetMAX_LEVEL(int MAX_LEVEL) {}
+    int skipList::GetLevel() const {}
+    void skipList::SetLevel(int level){}
+    Node* skipList::GetHead() const {}
+    void skipList::SetHead(Node* head) {}
+
+    Node* createNode(int, int);
+    void insertNode(int, int*);
 };
 
 skipList::skipList(int max_level, float p){
-    
-
+    this->MAX_LEVEL = max_level;
+    this->p = p;
+    level = 0;
+    head = new Node(INT_MIN, max_level);
 }
 
 skipList::~skipList() {
     delete head;
+}
+
+float skipList::GetP() const {
+    return p;
+}
+
+void skipList::SetP(float p) {
+    p = p;
+}
+
+int skipList::GetMAX_LEVEL() const {
+    return MAX_LEVEL;
+}
+
+void skipList::SetMAX_LEVEL(int MAX_LEVEL) {
+    MAX_LEVEL = MAX_LEVEL;
+}
+
+int skipList::GetLevel() const {
+    return level;
+}
+
+void skipList::SetLevel(int level) {
+    level = level;
+}
+
+Node* skipList::GetHead() const {
+    return head;
+}
+
+void skipList::SetHead(Node* head) {
+    head = head;
+}
+
+Node* skipList::createNode(int key, int level){
+    Node* newNode = new Node(key, level);
+    return newNode;
+}
+
+void skipList::insertNode(int key, int* value){
+    Node* cur = head;
+    Node* update[MAX_LEVEL+1];
+    memset(update, 0, sizeof(Node*)*(MAX_LEVEL+1));
+
+    for(int pos=level; pos>=0; pos--){
+
+    }
 }
 #endif
--- SkipList/C++/headers/skipListNode.h
+++ SkipList/C++/headers/skipListNode.h
@@ -2,12 +2,20 @@
 #define SKIP_LIST_NODE_H
 
 class listNode{
+private:
     int key;
     int *value;
     listNode** next;
+    int GetKey() const{}
+    void SetKey(int key) {}
+    int *GetValue() const {}
+    void SetValue(int *value){}
+    listNode** GetNext() const {}
+    void SetNext(listNode** next){}
+
 public:
     listNode(int, int );
-    ~listNode(); 
+    ~listNode();   
 };
 
 listNode::listNode(int key, int level) {
@@ -21,4 +29,30 @@ listNode::~listNode() {
         delete[] next;
         delete value;
 }
+
+int listNode::GetKey() const {
+    return key;
+}
+
+void listNode::SetKey(int key) {
+    key = key;
+}
+
+int* listNode::GetValue() const{
+    return value;
+}
+
+void listNode::SetValue(int *value) {
+    value = value;
+}
+
+listNode** listNode:: GetNext() const {
+    return next;
+}
+
+void listNode::SetNext(listNode** next) {
+    next = next;
+}
+
+
 #endif
